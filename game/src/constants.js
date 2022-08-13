2f654a89ed3a768ef512622e2257af43f105606d
Tessa <tessamero@gmail.com>
2022-08-13 13:33:25 -0700

    Update constants.js

--- game/src/constants.js
+++ game/src/constants.js
@@ -2,10 +2,5 @@ const gravity = new BABYLON.Vector2(0, -9.8);
 const flightForce = 5; // The amount of force applied when the "fly" button is pressed
 
 const greetings = [
-    'It\'s a bird!',
-    'It\'s a plane!',
-    '1000% more Cube!',
-    'A Whole New Dimension!',
-    'Are These Voxels?',
     'Hello, Turtle'
 ];
